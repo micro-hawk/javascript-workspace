9eba8c286a4e594c1d853da40beaaab9ce5b481d
micro-hawk <dasvikas0071@gmail.com>
2021-08-08 15:15:27 +0530

    refactor scss and css

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,4 @@
 const menuButton = document.querySelector('.menu-btn');
-// const clicked = (event) => {
-
-// }
 
 const toggleNav = function () {
     const opt_list = document.getElementById('options_list')
@@ -11,14 +8,4 @@ const toggleNav = function () {
         opt_list.style.left = '0px'
     }
 }
-
-// const navbar = document.getElementById('nav');
-// let navTop = navbar.offsetTop;
-
-// const fixedNav = function() {
-//     if(window.scrollY >= navTop) navbar.classList.add('fixed');
-//     else navbar.classList.remove('fixed');
-// }
-
-// window.addEventListener('scroll', fixedNav);
 menuButton.addEventListener('click', toggleNav);
