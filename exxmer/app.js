87a85a241d56b1046c277c486d24223964c17661
micro-hawk <dasvikas0071@gmail.com>
2021-09-27 01:59:05 +0530

    something exxmer

--- exxmer/app.js
+++ exxmer/app.js
@@ -1,3 +1,4 @@
+// import 'data.json' 
 const express = require('express')
 const app = express()
 
@@ -17,3 +18,5 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
+
+
